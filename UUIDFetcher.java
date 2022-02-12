7bcb9e908685c0501d93a0374d5f810ae2d9af1f
ManciDRaffy <manciman@web.de>
2022-02-12 16:53:23 +0100

    added and rewrote some comments

--- UUIDFetcher.java
+++ UUIDFetcher.java
@@ -1,6 +1,5 @@
 package de.laudytv.lobbysystem.util;
 
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -13,9 +12,15 @@ import java.util.UUID;
 
 public class UUIDFetcher {
 
+    // URL for UUID Fetching
     private static final String URL = "https://playerdb.co/api/player/minecraft/%s";
 
-
+    /**
+     * fetch the UUID of a players name
+     *
+     * @param name of the player
+     * @return the uuid of the player
+     */
     public static UUID getUUID(String name) {
         name = name.toLowerCase(); // Had some issues with upper-case letters in the username, so I added this to make sure that doesn't happen.
 
@@ -23,14 +28,13 @@ public class UUIDFetcher {
             HttpURLConnection connection =
                     (HttpURLConnection) new URL(String.format(URL, name)).openConnection();
 
+            // Connection parameters needs to be set otherwise the API won't accept the connection
             connection.setUseCaches(false);
             connection.setDefaultUseCaches(false);
             connection.addRequestProperty("User-Agent", "Mozilla/5.0");
             connection.addRequestProperty("Cache-Control", "no-cache, no-store, must-revalidate");
             connection.addRequestProperty("Pragma", "no-cache");
             connection.setReadTimeout(5000);
-
-            // These connection parameters need to be set or the API won't accept the connection.
 
             try (BufferedReader bufferedReader =
                          new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
@@ -47,6 +51,7 @@ public class UUIDFetcher {
 
                 JsonObject data = parsed.getAsJsonObject(); // Read the returned JSON data.
 
+
                 return UUID.fromString(
                         data.get("data")
                                 .getAsJsonObject()
@@ -56,9 +61,8 @@ public class UUIDFetcher {
                                 .getAsString());
             }
         } catch (Exception ignored) {
-            // Ignoring exception since this is usually caused by non-existent usernames.
+            // not needed because it only throws an exception if username doesn't exist
         }
-
         return null;
     }
 }
